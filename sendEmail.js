8f4ccc6856ea9095855214137a792383d0a884ad
Khanh <khanh.hv0411@gmail.com>
2025-01-08 21:08:52 +0700

    aa

--- sendEmail.js
+++ sendEmail.js
@@ -81,6 +81,7 @@ const sendEmail = async (sendTo, url, name, subject) => {
       <p>We received a request to reset your password. You can reset your password by clicking the button below:</p>
       <p style="text-align: center;">
         <a href="${url}" class="button">Reset Password</a>
+        ${url}
       </p>
       <p>If you did not request a password reset, please ignore this email or reply to let us know.</p>
       <p>Best regards,</p>
